dc2a78003940c4333f1041ae5d4ba96e2734ea17
Jair de Sousa <jairsmarques1126@gmail.com>
2024-10-03 09:07:17 -0300

    adicionando o método da aula sobre toString

--- introducao-orientacao-objetos/src/cursojava/executavel/PrimeiraClasseJava.java
+++ introducao-orientacao-objetos/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,13 +6,7 @@ import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
 
-	/*Main é um método auto executável em Java*/
 	public static void main(String[] args) {
-		
-		/*Objeto ainda não existe na memória*/
-		Aluno aluno1;
-		/*new Aluno() é uma instancia(Criação de Objeto)*/
-		/*aluno1 é uma referencia para o objeto aluno*/
 		
 		String nome = JOptionPane.showInputDialog("Qual o seu nome:");
 		String idade = JOptionPane.showInputDialog("Qual a idade:");
@@ -29,9 +23,8 @@ public class PrimeiraClasseJava {
 		String nota3 = JOptionPane.showInputDialog("Nota3?:");
 		String nota4 = JOptionPane.showInputDialog("Nota4?:");
 		
-		aluno1 = new Aluno();/*Aqui será o João*/
+		Aluno aluno1 = new Aluno();
 		
-		/*settando os atributos*/
 		aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascimento);
@@ -46,21 +39,10 @@ public class PrimeiraClasseJava {
 		aluno1.setNota2(Double.parseDouble(nota2));
 		aluno1.setNota3(Double.parseDouble(nota3));
 		aluno1.setNota4(Double.parseDouble(nota4));
-		
-		 /*recupera os dados*/
-		System.out.println("Nome é = " + aluno1.getNome());
-		System.out.println("Idade é = " + aluno1.getIdade());
-		System.out.println("Nascimento = " + aluno1.getDataNascimento());
-		System.out.println("RG = " + aluno1.getRegistroGeral());
-		System.out.println("CPF = " + aluno1.getNumeroCpf());
-		System.out.println("Nome mãe = " + aluno1.getNomeMae());
-		System.out.println("Nome pai = " + aluno1.getNomePai());
-		System.out.println("Media da nota é = " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-		System.out.println("Resultado2 = " + aluno1.getAlunoAprovado2());
-		System.out.println("-------------------------------------------");
-		/*======================================================================*/
-		
+	
+		System.out.println(aluno1.toString());/*Descrição do objeto na memória*/
+		System.out.println("Média do aluno = " + aluno1.getMediaNota());
+		System.out.println("Resultado" + aluno1.getAlunoAprovado2());
 	}
 
 }
