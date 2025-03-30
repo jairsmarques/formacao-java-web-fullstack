17adfb574051b9384f93c0ba07d23c1a43481a60
Jair Marques <jairsmarques1126@gmail.com>
2025-03-30 11:59:14 -0300

    Criando o repositório do curso

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/* Main é um método auto executável em Java */
+	public static void main(String[] args) {
+		
+		/* Escreve testo no console */
+		System.out.println("Meu primeiro programa em Java - do Curso Formação Java Web");
+
+	}
+
+}
